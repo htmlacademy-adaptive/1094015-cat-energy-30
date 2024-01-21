8adb38b7e00f2af2675a5d25d0abcf6d185e286a
Ksenia <ksjusha00@mail.ru>
2024-01-21 23:09:10 +0300

    Дорабатывает сетки и графику

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,25 @@
-/* в этот файл добавляет скрипты*/
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('toggle').addEventListener('click', () => {
+    document.querySelector('.header').classList.toggle('open');
+  });
+});
+
+// Закрыть меню при нажатии на Esc
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    document.querySelector('.header').classList.remove('open');
+  }
+});
+
+// Закрыть меню при клике вне его
+document.getElementById('nav').addEventListener('click', (event) => {
+  event._isClickWithInMenu = true;
+});
+document.getElementById('toggle').addEventListener('click', (event) => {
+  event._isClickWithInMenu = true;
+});
+document.body.addEventListener('click', (event) => {
+  if (event._isClickWithInMenu) {
+    document.querySelector('.header').classList.remove('open');
+  }
+});
