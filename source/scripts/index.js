70caadc2b59ba38c8d3ddc1dbe10d321a18d04ff
Ksenia <ksjusha00@mail.ru>
2024-01-26 06:23:39 +0300

    Добавляет карту

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,12 +1,1 @@
-const navHeader = document.querySelector('header');
-const navToggle = document.querySelector('header__toggle');
 
-navHeader.classList.remove('header__toggle--nojs');
-navToggle.classList.remove('nav--nojs');
-
-navToggle.addEventListener('click', () => {
-  navToggle.classList.toggle('header__toggle--close');
-  navToggle.classList.toggle('toggle__toggle--open');
-  navToggle.classList.toggle('open');
-  navToggle.classList.toggle('close');
-});
